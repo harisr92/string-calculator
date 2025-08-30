08eccfa4aa58e66ac0b4baca7c9ecee5a1f07d48
Hari N <harikri@protonmail.com>
2025-08-30 13:09:26 +0530

    feat: decouple parsing and calculation, add Calculator clas

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,22 +1,18 @@
 # frozen_string_literal: true
 
 require_relative 'string_calculator/parser/string_parser'
+require_relative 'string_calculator/calculator'
 require_relative 'string_calculator/runner'
 
 module StringCalculator
   VERSION = '0.0.1'
 
   class << self
-    def add(numbers)
-      return 0 if numbers.empty?
+    def add(input)
+      parser = Parser::StringParser.new(input)
+      calculator = Calculator.new
 
-      parsed_numbers = parse(numbers)
-      Runner.run(parsed_numbers)
-    end
-
-    def parse(numbers)
-      parser = Parser::StringParser.new(numbers)
-      parser.parse
+      Runner.new(parser: parser, calculator: calculator).execute
     end
   end
 end
