e919d7c1818b04f52051ecdc279fc463aef1dedc
Hari N <harikri@protonmail.com>
2025-08-30 16:28:35 +0530

    spec: add support for multi-character custom delimiters in specs

--- spec/string_calculator/parser/delimiter_parser_spec.rb
+++ spec/string_calculator/parser/delimiter_parser_spec.rb
@@ -6,5 +6,12 @@ RSpec.describe StringCalculator::Parser::DelimiterParser do
       parser = described_class.new("//;\n1;2;3")
       expect(parser.parse).to eq([';', "\n"])
     end
+
+    context 'with multi-character custom delimiter' do
+      it 'parses a string with multi-character custom delimiter' do
+        parser = described_class.new("//[***]\n1***2***3")
+        expect(parser.parse).to eq(['***', "\n"])
+      end
+    end
   end
 end
